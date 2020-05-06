b786b6fa208962842a2b1c110ccdbf56140ecdfc
yngtodd <young.todd.mk@gmail.com>
2020-05-06 00:43:19 -0400

    Add error checking
    Lots of things can go wrong when you read in a file.

--- src/main.c
+++ src/main.c
@@ -39,12 +39,32 @@ static char* file_read(const char* path)
     //TODO(TODD): error checking 
     FILE* file = fopen(path, "rb");
 
+    if (file == NULL)
+    {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
     fseek(file, 0L, SEEK_END);
     size_t file_size = ftell(file);
     rewind(file);
 
     char* buffer = (char*)malloc(file_size + 1);
+
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);<Paste>
+        exit(74);
+    }
+
     size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+
+    if (bytes_read < file_size)
+    {
+        fprintf(stderr, "Could not read file \"%s\".\n", path); 
+        exit(74)
+    }
+
     buffer[bytes_read] = '\0';
 
     fclose(file);
